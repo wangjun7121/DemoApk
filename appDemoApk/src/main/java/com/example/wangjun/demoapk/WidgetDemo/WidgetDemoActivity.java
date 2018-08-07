1ebea4e9ea51f024e90213825f037b2f1bacad28
wangjun <840788021@qq.com>
2018-08-07 10:30:59 +0800

    修改 WidgetDemo 使用动态添加按钮方式

--- appDemoApk/src/main/java/com/example/wangjun/demoapk/WidgetDemo/WidgetDemoActivity.java
+++ appDemoApk/src/main/java/com/example/wangjun/demoapk/WidgetDemo/WidgetDemoActivity.java
@@ -1,90 +1,107 @@
 package com.example.wangjun.demoapk.WidgetDemo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.example.wangjun.demoapk.HardwareDemo.HardwareDemoActivity;
 import com.example.wangjun.demoapk.R;
+import com.example.wangjun.demoapk.SensorDemo.CAlsActivity;
+import com.example.wangjun.demoapk.Tools.LogUtil;
 
 
 public class WidgetDemoActivity extends AppCompatActivity {
 
     private static String sLogcatTAG = "ActivityDemoActivity";
-
+    Context m_Context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.widgetdemo);
+        setContentView(R.layout.scrollview_entry);
+        m_Context = this;
+        final LinearLayout layout = (LinearLayout) this.findViewById(R.id.linearlayout);
+        layout.setOrientation(LinearLayout.VERTICAL);
 
-        Button m_testWidgetBtn;
-        m_testWidgetBtn = (Button) findViewById(R.id.m_testWidgetBtn);
-        m_testWidgetBtn.setOnClickListener(new View.OnClickListener() {
+
+        Button testWidgetBtn = new Button(m_Context);
+        testWidgetBtn.setText("常用控件测试");
+        layout.addView(testWidgetBtn);
+        testWidgetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CWidgetActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button m_dynamicAddBtn;
-        m_dynamicAddBtn = (Button) findViewById(R.id.m_dynamicAddBtn);
-        m_dynamicAddBtn.setOnClickListener(new View.OnClickListener() {
+        Button dynamicAddBtn = new Button(m_Context);
+        dynamicAddBtn.setText("动态添加 Button 测试");
+        layout.addView(dynamicAddBtn);
+        dynamicAddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CDynamidAddButtonActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button m_widgetInheritBtn;
-        m_widgetInheritBtn = (Button) findViewById(R.id.m_widgetInheritBtn);
-        m_widgetInheritBtn.setOnClickListener(new View.OnClickListener() {
+        Button widgetInheritBtn = new Button(m_Context);
+        widgetInheritBtn.setText("Widget 继承关系图");
+        layout.addView(widgetInheritBtn);
+        widgetInheritBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CWidgetInheritActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button m_customWidgetBtn;
-        m_customWidgetBtn = (Button) findViewById(R.id.m_customWidgetBtn);
-        m_customWidgetBtn.setOnClickListener(new View.OnClickListener() {
+        Button customWidgetBtn = new Button(m_Context);
+        customWidgetBtn.setText("自定义 Widget 测试");
+        layout.addView(customWidgetBtn);
+        customWidgetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CCustomWidgetActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button m_testWidgetSizeBtn;
-        m_testWidgetSizeBtn = (Button) findViewById(R.id.m_testWidgetSizeBtn);
-        m_testWidgetSizeBtn.setOnClickListener(new View.OnClickListener() {
+        Button testWidgetSizeBtn = new Button(m_Context);
+        testWidgetSizeBtn.setText("控件尺寸：dp/sp");
+        layout.addView(testWidgetSizeBtn);
+        testWidgetSizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CWidgetSizeActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button m_testTextViewBtn;
-        m_testTextViewBtn = (Button) findViewById(R.id.m_testTextViewBtn);
-        m_testTextViewBtn.setOnClickListener(new View.OnClickListener() {
+        Button testTextViewBtn = new Button(m_Context);
+        testTextViewBtn.setText("TextView 格式化");
+        layout.addView(testTextViewBtn);
+        testTextViewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(sLogcatTAG, "testArrayAdapterBtn");
+                LogUtil.d("debug\n");
                 Intent intent = new Intent(WidgetDemoActivity.this, CTextViewActivity.class);
                 startActivity(intent);
             }
         });
+
+
 
     }
 
